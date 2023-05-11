6e247e352cc8118c64e7a803a6c97a432c1994d7
Antonio Lima Goes Junior <ajlima8@gmail.com>
2023-05-11 15:29:23 -0300

    Biblioteca faker

--- cypress-express-mark/cypress/e2e/tasks.cy.js
+++ cypress-express-mark/cypress/e2e/tasks.cy.js
@@ -1,17 +1,19 @@
 /// <reference types ="cypress"/>
 
+import { faker } from '@faker-js/faker';
+
 describe('tarefas', () => {
 
     it('deve cadastrar uma nova tarefa', () => {
         cy.visit('http://localhost:8080')
 
         cy.get('input[placeholder="Add a new Task"]')
-            .type('junior')
+            .type(faker.music.songName())
 
         ////xpath button[text()="Create "]
 
         cy.contains('button', 'Create').click()
-        cy.get('button[class="_listItemDeleteButton_1kgm5_52"]').click()
+        //cy.get('button[class="_listItemDeleteButton_1kgm5_52"]').click()
 
     })
 })
