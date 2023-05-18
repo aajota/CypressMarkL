29467c7f40169a0be0904147e06a615fae6e5c97
Antonio Lima Goes Junior <ajlima8@gmail.com>
2023-05-18 06:06:57 -0300

    criando commands

--- cypress-express-mark/cypress/e2e/tasks.cy.js
+++ cypress-express-mark/cypress/e2e/tasks.cy.js
@@ -10,25 +10,20 @@ describe('tarefas', () => {
 
     it('deve cadastrar uma nova tarefa', () => {
 
-        cy.request({
-            url: 'http://localhost:3333/helper/tasks',
-            method: 'DELETE',
-            body: { name: taskName }
-        }).then(response => {
-            expect(response.status).to.eq(204)
-        })
+        cy.removerTaskByName(taskName)
+        cy.createTask(taskName)
 
-        cy.visit('http://localhost:8080')
+        //cy.visit('http://localhost:8080')
 
         // cy.get('input[placeholder="Add a new Task"]')
         //     .type(faker.music.songName())
 
-        cy.get('input[placeholder="Add a new Task"]')
-            .type(taskName)
+        // cy.get('input[placeholder="Add a new Task"]')
+        //     .type(taskName)
 
         ////xpath button[text()="Create "]
 
-        cy.contains('button', 'Create').click()
+        // cy.contains('button', 'Create').click()
         //cy.get('button[class="_listItemDeleteButton_1kgm5_52"]').click()
 
         // cy.get('main div p').should('be.visible').should('have.text', 'junior')
@@ -38,40 +33,21 @@ describe('tarefas', () => {
 
     })
 
-    it('não deve permitir tarefa duplicada', ()=>{
+    it('não deve permitir tarefa duplicada', () => {
 
         const task = {
-            name:   'Antonio',
+            name: 'Antonio',
             is_done: false
         }
-      
 
-        cy.request({
-            url: 'http://localhost:3333/helper/tasks',
-            method: 'DELETE',
-            body: { name: task.name}
-        }).then(response => {
-            expect(response.status).to.eq(204)
-        })
-        cy.request({
-            url: 'http://localhost:3333/tasks',
-            method: 'POST',
-            body: task
-
-        }).then(response => {
-            expect(response.status).to.eq(201)
-        })
-
-        cy.visit('http://localhost:8080')
-
-        cy.get('input[placeholder="Add a new Task"]')
-            .type(task.name)
-
-        cy.contains('button', 'Create').click()
+        cy.removerTaskByName(task.name)
+        cy.postTask(task)
+        cy.createTask(task.name)
 
         cy.get('.swal2-html-container')
-        .should('be.visible')
-        .should('have.text', 'Task already exists!')
+            .should('be.visible')
+            .should('have.text', 'Task already exists!')
 
     })
 })
+
